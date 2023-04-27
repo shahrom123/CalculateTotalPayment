c918e01f6fe8712a6a3067ab7aa3a12c83263b95
Shahrom_developer <shahrom909050409@gmail.com>
2023-04-27 16:44:12 +0500

    first commit

---
+++ intallment/Order.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace intallment
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public string Product { get; set; }
+        public decimal Amount { get; set; }
+        public string PhoneNumber { get; set; }
+        public int InstallmentDuration { get; set; }
+
+        public Order() { }
+        
+      public decimal CalculateTotalPayment(string product, decimal amount, string phoneNumber,int installmentDuration)
+        {
+            decimal TotalAmount = amount;
+
+           if(product == "Смартфон")
+            {
+                if(installmentDuration > 9)
+                {
+                    TotalAmount+=(TotalAmount * 0.03m);
+                }
+            }
+           else if(product == "Компьютер")
+            {
+                if(installmentDuration > 12)
+                {
+                    TotalAmount+= (TotalAmount * 0.04m); 
+                }
+            }
+           else if(product == "Телевизор")
+            {
+                if(installmentDuration > 18)
+                {
+                    TotalAmount+= (TotalAmount * 0.05m);
+
+                }
+            }
+            SendSMS(phoneNumber,
+                $"Вы приобрели {product} на сумму {TotalAmount} сомони в рассрочку на {installmentDuration} месяцев." +"\n" +
+                $"Общая сумма платежа {TotalAmount} сомони ");
+
+           return TotalAmount;            
+   }
+
+        public void SendSMS(string phoneNumber, string message)
+        {
+
+            Console.WriteLine($"Отправлено СМС на номер {phoneNumber}:");
+            Console.WriteLine(message);
+        }
+
+}
+}
---
+++ intallment/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using intallment;
+
+var order1 = new Order();
+
+Console.WriteLine(order1.CalculateTotalPayment("Смартфон", 1000, "+992909050409", 9));
+Console.WriteLine();
+
+Console.WriteLine(order1.CalculateTotalPayment("Компьютер", 1000, "+992909050409", 18));
+Console.WriteLine();
+
+Console.WriteLine(order1.CalculateTotalPayment("Телевизор", 1000, "+992909050409", 24));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
